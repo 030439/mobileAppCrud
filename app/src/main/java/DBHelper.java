0d8615ede2e1d7514fa994e111e3af1a35ec6a2d
030439 <praimchand763@gmail.com>
2022-11-26 18:51:53 +0500

    insert method creatd

---
+++ app/src/main/java/DBHelper.java
@@ -0,0 +1,28 @@
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper
+{
+    public DBHelper(@Nullable Context context) {
+        super(context, "CrudDatabase.db",null,1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase DB) {
+        DB.execSQL("create table crudApp (SN Int primary key, Name Text,Contact Text,DOB Text)");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase DB, int i, int i1) {
+    DB.execSQL("drop table if exists crudApp");
+    }
+    public boolean insertData(String name,String contact,String dob){
+        SQLiteDatabase DB=this.getWritableDatabase();
+        ContentValues contentvalues=new ContentValues();
+    }
+}
