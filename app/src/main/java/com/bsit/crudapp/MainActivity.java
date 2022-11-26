e59388fc37f59de7f17fac32fd6c2dfac4a5779e
030439 <praimchand763@gmail.com>
2022-11-26 18:16:15 +0500

    buttons and txt views variables set

--- app/src/main/java/com/bsit/crudapp/MainActivity.java
+++ app/src/main/java/com/bsit/crudapp/MainActivity.java
@@ -3,12 +3,19 @@ package com.bsit.crudapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnInsert,btnUpdate,btnDelete,btnView;
+    EditText txtName,txtContact,txtDob;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtName=findViewById(R.id.name);
+        txtContact=findViewById(R.id.contact);
+        txtDob=findViewById(R.id.dob);
     }
 }
